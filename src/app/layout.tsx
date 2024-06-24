b8af807c71075f2740d7b88ed2878f4666dad470
watermarkmarketing <creative@watermarkexperience.com>
2024-06-24 17:52:02 +0530

    Added Speed Insights in WMM
    WMM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: "Watermark Experience",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+      <SpeedInsights />
       <Analytics />
       </body>
     </html>
