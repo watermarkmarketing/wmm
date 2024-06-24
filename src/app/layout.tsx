32c875d1be971e9272587e1e340c1b76401a6449
watermarkmarketing <creative@watermarkexperience.com>
2024-06-24 17:45:24 +0530

    Update layout page
    Change react to next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Watermark Experience",
