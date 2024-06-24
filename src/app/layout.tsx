f56d6f174cc0296795e17a8b115ee47eac9946ff
watermarkmarketing <creative@watermarkexperience.com>
2024-06-24 17:56:26 +0530

    Next to react
    WMM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export const metadata: Metadata = {
   title: "Watermark Experience",
