383e3ff141de76aa235d50cacd642193ab364825
watermarkmarketing <creative@watermarkexperience.com>
2024-06-24 17:47:32 +0530

    update layout tsx
    revert the previous changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Watermark Experience",
