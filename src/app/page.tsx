06879b9cf04fa5be15172e04a7d1d79ffe09bf17
Watermark Marketing <creative@watermarkexperience.com>
2025-06-10 11:31:49 +0530

    added WMDigital2 in homepage for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,12 @@ const servicesData = [
     iconSrcBlk: "/wmdigital-black.svg",
     link: "/service/wm-digital",
   },
+  {
+    title: "WMDigital2",
+    iconSrc: "/wmdigital.svg",
+    iconSrcBlk: "/wmdigital-black.svg",
+    link: "/service/wm-digital2",
+  },
 ];
 
 export default function Home() {
