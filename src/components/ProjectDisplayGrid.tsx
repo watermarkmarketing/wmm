a9b0c35950a5c8db24151958c645869b0f3502f0
Watermark Marketing <166810003+watermarkmarketing@users.noreply.github.com>
2025-10-28 18:34:27 +0530

    Refactor imports in ProjectDisplayGrid component
    Updated imports to include NextJsImage and react-photo-album, and removed unused imports.

--- src/components/ProjectDisplayGrid.tsx
+++ src/components/ProjectDisplayGrid.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import React, { useEffect, useRef } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import ServiceFooter from '@/components/Service/ServiceFooter'
-import Gallery from '@/components/Gallery'
-import { gsap } from "gsap";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import NextJsImage from '@/components/NextJsImage';
+import PhotoAlbum from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 
 interface ProjectData {
